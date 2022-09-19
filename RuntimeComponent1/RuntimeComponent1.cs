5e6ab0140c5c383136b661705ce3a84733cdc6d2
shivammcst <shivamprasad@microsoft.com>
2022-09-19 12:52:10 +0530

    Add project files.

---
+++ RuntimeComponent1/RuntimeComponent1.cs
@@ -0,0 +1,13 @@
+﻿// ExampleBackgroundTask.cs
+using Windows.ApplicationModel.Background;
+
+namespace Tasks
+{
+    public sealed class RuntimeComponent1 : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            System.Console.WriteLine("background task event received");
+        }
+    }
+}
